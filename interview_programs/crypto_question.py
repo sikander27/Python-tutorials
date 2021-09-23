5a0541230ff7fe4ff43e6b9c3ae78a53d99332cb
sikander27 <sikander.khan@nutanix.com>
2021-09-23 15:15:34 +0530

    interview programs deleted

--- interview_programs/crypto_question.py
+++
@@ -1,9 +0,0 @@
-#question.py
-# msg = input("enter your secret message: ")
-msg = "sikander is doing good"
-x = 'abcdefghijklmnopqrstuvwxyz'
-y = x[::-1]
-print(x)
-print(y)
-tran = msg.maketrans(x, y)
-print(msg.translate(tran))
