8bf567bf0f0c05c85f46c2de3624ba9a84e59f22
sikander27 <sikander.khan@nutanix.com>
2021-09-23 21:21:37 +0530

    interview questions for wonderhood

--- competitive-programming/productOf3DivisibleBy9.py
+++ competitive-programming/productOf3DivisibleBy9.py
@@ -5,37 +5,24 @@
 # 7 4 7 -1
 # Output
 # -1 3 3
-
+# Coded by sikander khan (sikander27/imsikandarkhan@outlook.com)
 
 def findx(x, y, f=0):
     """
     x = list A
     y = list B
     f = flag to check if y is a subset of x
+    
     """
-    print(x)
-    print(y)
     for i in range(len(x)):
         for j in range(len(y)):
-            print(x[i], y[j])
             if (x[i] * y[j]) % 9 == 0:
-                print(x[i], y[j],i,j)
                 if f and j != i-1:
-                    print("************")
-                    print(x[i], y[j],i,j)
-                    print("************")
                     return list((x.pop(i), x.pop(j+1))) 
                 if j != i -1:
                     return list((x.pop(i), y.pop(j)))
     return []
 
-# def findz(x):
-#     for i in range(0, len(x)-1):
-#         for j in range(i + 1, len(x)):
-#             if (x[i] * x[j]) % 9 == 0:
-#                 return list((x.pop(i), x.pop(j)))
-#     return []
-            
 def findy(x, y):
     for i in range(len(y)):
         if (x[0]*x[1]*y[i]) % 9 == 0:
@@ -51,22 +38,15 @@ def main():
         return -1
     A.sort()
     B.sort()
-    # import pdb; pdb.set_trace()
     pair.extend(findx(A, A[1:], 1))
-    print("After a -> {}".format(pair))
     if len(pair) < 2:
         pair.extend(findx(B, B[1:], 1))
-    print("After b -> {}".format(pair))
     if len(pair) < 2:
         pair.extend(findx(A, B))
-    print("After a,b -> {}".format(pair))
     if len(pair) == 2:
         pair.append(findy(pair, A))
     if len(pair) == 2:
         pair.append(findy(pair, B))
-    
-    print(A)
-    print(B)
     if len(pair) < 3:
         return -1
     return " ".join(map(str,pair))
